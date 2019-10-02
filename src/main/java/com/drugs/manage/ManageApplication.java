977686b8606ec5a6cfb5c3d03d6ff2190744ab7c
zyliu <891990586@qq.com>
2019-10-02 16:03:09 +0800

    修改打包

--- src/main/java/com/drugs/manage/ManageApplication.java
+++ src/main/java/com/drugs/manage/ManageApplication.java
@@ -3,13 +3,20 @@ package com.drugs.manage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.drugs.manage.mapper")
-public class ManageApplication {
+public class ManageApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(ManageApplication.class);
+    }
 }
